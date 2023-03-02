f862dc13fc8137aa33157425844f3282e189c28c
midnight33 <gra33@bk.ru>
2023-03-02 19:53:39 +0300

    Задачка 3 - готова

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 8)
+{
+   if (day==6 || day==7)
+   {
+    Console.WriteLine("УРА, ВЫХОДНОЙ");
+   }
+   else
+   {
+    System.Console.WriteLine("Увы - Раобчий день");
+   }
+}
+else
+{
+    System.Console.WriteLine("Нет такого дня недели");
+}
